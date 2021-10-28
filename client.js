e3c3f2ffcc180cfcda612d36476942f26af49a1a
Sarah Helgen <sarah.helgen@gmail.com>
2021-10-28 16:42:27 -0500

    another 10/28/21 update

--- client.js
+++ client.js
@@ -136,3 +136,16 @@ function convertToSeconds( hours, minutes ) {
     let totalConversation = conversationOfMinutes + conversionofHours;
     return totalConversation;
 }
+
+//Create a function that takes two arguments. Both arguments are 
+//integers, a and b. Return true if one of them is 10 or if their sum is 10.
+
+function sumTen ( a, b ){
+    if (a == 10 || b == 10 ){
+        return true;
+    } else if ( a + b == 10){
+        return true;
+    } else{
+        return false;
+    }
+}
